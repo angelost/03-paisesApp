9d7c4cb359307e760bf2245bc799a06179f00c78
Angelo <angelo.gonzalez@live.com>
2022-08-05 16:58:01 -0400

    128. Mostrar autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,10 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li {
+      cursor: pointer;
+    }`
   ]
 })
 export class PorPaisComponent {
@@ -13,10 +17,13 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[]= [];
+  paisesSugeridos: Country[]= [];
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService) { }
 
   buscar( termino: string ){
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -38,6 +45,22 @@ export class PorPaisComponent {
   
   sugerencias( termino: string ) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+      .subscribe({
+        next: (paises) => {
+          this.paisesSugeridos = paises
+        },
+        error: (err) => {
+          this.paisesSugeridos = []
+        }
+      });
+  }
+
+  buscarSugerido( termino: string) {
+    this.buscar( termino );
   }
 
 }
